ba19d9c2cb0f9354310055b2155f0a9f7009a8d7
rawbeen248 <rawbeendraw.248@gmail.com>
2023-08-01 12:25:26 +0545

    Switched to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage";
 import Projects from "./Projects"
 import AboutMe from "./AboutMe";
